2c4f47fc544a1f8e7246026086d203b07018e4fb
Raunak Dey <91902527+rdey27@users.noreply.github.com>
2023-01-04 22:13:15 +0530

    Update entryform.py

--- entryform.py
+++ entryform.py
@@ -33,7 +33,20 @@ def Clear():
    address.delete(0,END)
    Department.set("")
 
+
+
+
+# Headding level
+
+# Create a heading label
+heading_label = Label(window, text="Data Entry project By GROUP 6", font=("Arial", 20),activebackground="Blue", bg="Blue", fg="White")
+
+# Center the alignment of the heading label
+heading_label.grid(row=0,column = 25, padx= 20, pady=10)
+
+
 # 3 labels, 4 buttons,3 entry fields
+
 label1=Label(window,text="Name: ",padx=39,pady=10,background="white",foreground="black")
 label2=Label(window,text="Age: ",padx=43.9,pady=10,background="white",foreground="black")
 label3=Label(window,text="Contact: ",padx=34,pady=10,background="white",foreground="black")
@@ -51,34 +64,35 @@ address = Entry(window,width = 30 , borderwidth = 4)
 # Department = Entry(window,width = 30,borderwidth = 3)
 Department = StringVar(window)
 Department.set("Select Your Department")
-drop_menu = OptionMenu(window, Department,"Computer Science and Engineering", "Information Technology","Python","JavaScript","Rust","GoLang")
+drop_menu = OptionMenu(window, Department,"Computer Science and Engineering", "Information Technology","Electronics and Communication","Electrical","Artificial Intelligence")
 #drop.pack()
 
-save=Button(window,text="Save",font="Arial 12",padx=15,pady=5,background="Blue",foreground="White",command=Save)
-add=Button(window,text="Add",font="Arial 12",padx=15,pady=5,background="Blue",foreground="White",command=Add)
-clear=Button(window,text="Clear",font = "Arial 12",padx=15,pady=5,background="Blue",foreground="White",command=Clear)
-Exit=Button(window,text="Exit",font="Arial 12",padx=15,pady=5,background="Blue",foreground="White",command=window.quit)
+save=Button(window,text="Save",font="Arial 12",padx=7,pady=5,background="Blue",foreground="White",command=Save)
+add=Button(window,text="Add",font="Arial 12",padx=7,pady=5,background="Blue",foreground="White",command=Add)
+clear=Button(window,text="Clear",font = "Arial 12",padx=7,pady=5,background="Blue",foreground="White",command=Clear)
+Exit=Button(window,text="Exit",font="Arial 12",padx=7,pady=5,background="Blue",foreground="White",command=window.quit)
 
-label1.grid(row=0,column=20)
-label2.grid(row=1,column=20)
-label3.grid(row=2,column=20)
-label4.grid(row =3, column=20)
-label5.grid(row = 4,column=20)
+label1.grid(row=10,column=20)
+label2.grid(row=11,column=20)
+label3.grid(row=12,column=20)
+label4.grid(row =13, column=20)
+label5.grid(row = 14,column=20)
 
-name.grid(row=0,column=25)
-age.grid(row=1,column=25)
-contact.grid(row=2,column=25)
-address.grid(row=3,column=25)
-drop_menu.grid(row=4,column=25)
-save.grid(row=14,column=27,columnspan=2)
-add.grid(row=14,column=25,columnspan=2)
-clear.grid(row=18,column=27,columnspan=2)
-Exit.grid(row=18,column=25,columnspan=2)
+name.grid(row=10,column=25)
+age.grid(row=11,column=25)
+contact.grid(row=12,column=25)
+address.grid(row=13,column=25)
+drop_menu.grid(row=14,column=25)
+save.grid(row=24,column=26,columnspan=2)
+add.grid(row=24,column=25,columnspan=2)
+clear.grid(row=26,column=26,columnspan=2)
+Exit.grid(row=26,column=25,columnspan=2)
 
 
 window.mainloop()
 print(list)
 print(main_lst)
+
 
 
 
